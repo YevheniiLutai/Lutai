666a9cbc1f12b8050d2b543fc6208b270b2dd13a
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2019-12-16 23:50:59 +0200

    Fixed error in FileStatsTest laba4

--- laba5/test/StackTest.java
+++ laba5/test/StackTest.java
@@ -15,6 +15,7 @@ public class StackTest {
 	@Test
 	public void testPushElementOntoEmptyStack() {
 		intStack.push(234);
+		assertThat(intStack.pop(), is(234));
 	}
 
 	@Test(expected = EmptyStackException.class)
